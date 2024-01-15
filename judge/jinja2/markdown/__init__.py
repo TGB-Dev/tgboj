900d33cd74e6fa468f1c5115a0186cae70163395
Binh Minh <minhngoc2891211@gmail.com>
2024-01-16 00:02:23 +0700

    Fix details doesn't open by default  - Add 'open' attribute for bleach cleaner.

--- judge/jinja2/markdown/__init__.py
+++ judge/jinja2/markdown/__init__.py
@@ -118,6 +118,7 @@ def markdown(text, style, math_engine=None, lazy_load=False, strip_paragraphs=Fa
         extras.append('nofollow')
 
     bleach_params = styles.get('bleach', {})
+    bleach_params['attributes']['details'] = ['open']
 
     post_processors = []
     if styles.get('use_camo', False) and camo_client is not None:
